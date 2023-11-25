4a498e11f6b7acd83f628d2a713489c8a584f1c6
Alfredo Aramburo <20031030@itcelaya.edu.mx>
2023-11-24 21:48:28 -0600

    Initial commit

---
+++ src/main/java/com/example/booster_back/BoosterBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.booster_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoosterBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoosterBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/booster_back/BoosterBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.booster_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoosterBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
